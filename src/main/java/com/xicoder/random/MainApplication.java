ae8b74fbeb200adf7596e0ac024b65ab3d8f67b8
Krishnan, Sai <Sai.Krishnan@elektrobit.com>
2021-05-22 21:46:17 +0530

    Added Main Application & test cases

---
+++ src/main/java/com/xicoder/random/MainApplication.java
@@ -0,0 +1,7 @@
+package com.xicoder.random;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.println(RandomGenerator.getRandomStringGenerated((14)));
+    }
+}
---
+++ src/test/java/com/xicoder/random/RandomGeneratorTest.java
@@ -0,0 +1,34 @@
+package com.xicoder.random;
+
+
+import org.junit.jupiter.api.*;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RandomGeneratorTest {
+
+    @DisplayName("Checking whether the random string length is given length")
+    @Test
+    public void lengthTest() {
+        int length = 12;
+        String randomString = RandomGenerator.getRandomStringGenerated(length);
+        Assertions.assertEquals(randomString.length(), length);
+    }
+
+    @DisplayName("Checking whether the random string contains only alphanumeric characters")
+    @Test
+    public void alphaNumericTest() {
+        String randomString = RandomGenerator.getRandomStringGenerated(12);
+        // Regex to check string is alphanumeric or not.
+        String regex = "^(?=.*[a-zA-Z])(?=.*[0-9])[A-Za-z0-9]+$";
+        // Compile the ReGex
+        Pattern p = Pattern.compile(regex);
+        // Pattern class contains matcher() method
+        // to find matching between given string
+        // and regular expression.
+        Matcher m = p.matcher(randomString);
+        // Should return true
+        Assertions.assertTrue(m.matches());
+    }
+}
