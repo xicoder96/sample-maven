d1568e615db8c5822a5d74aea4f1d877b71bde64
Krishnan, Sai <Sai.Krishnan@elektrobit.com>
2021-05-23 12:09:49 +0530

    fixed mvn errors

--- src/main/java/com/xicoder/random/MainApplication.java
+++
@@ -1,7 +0,0 @@
-package com.xicoder.random;
-
-public class MainApplication {
-    public static void main(String[] args) {
-        System.out.println(RandomGenerator.getRandomStringGenerated((14)));
-    }
-}
